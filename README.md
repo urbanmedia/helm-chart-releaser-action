a58375d52bc462fcb0a0d5469a8a6d14648e8023
Leon Steinhäuser <42437185+leonsteinhaeuser@users.noreply.github.com>
2023-02-21 16:23:17 +0100

    feature: helm chart release action (#1)
    * feat: implemented Helm chart release action
    
    * feat: added GitHub action interface configuration
    
    * docs: added how to use steps
    
    * fix: removed hard-coded chart registry repository

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # helm-chart-releaser-action
-This repository provides a GitHub action to publish Helm Charts to a Chart repository using the plain push operation provided by Helm.
+
+This repository provides a GitHub action for publishing Helm charts to a chart repository using the ***push*** command provided by Helm.
+
+## Input values
+
+| Input | Required | Default | Description |
+| --- | --- | --- | --- |
+| `gh_token` | `true` | | GitHub token to use for authentication. |
+| `github_actor` | `false` | `${{ github.actor }}` | GitHub actor to use for git operations (git tag). |
+| `charts_dir` | `false` | `charts` | Directory containing the Helm Charts. |
+| `chart_repository_url` | `true` | The URL of the chart repository to release the charts to. This is used to configure the Helm push plugin. Example: `oci://ghcr.io/username/repo`. |
+| `chart_repository_username` | `true` | | The username to use when authenticating to the chart repository. This is used to configure the Helm push plugin. |
+| `chart_repository_password` | `true` | The password to use when authenticating to the chart repository. This is used to configure the Helm push plugin. |
+
+### Environment variables
+
+| Variable | Required | Default | Description |
+| --- | --- | --- | --- |
+| `DEBUG` | `false` | `false` | Enable debug output. |
+
+## Flow of the action
+
+```mermaid
+graph TD;
+A[Action Call] --> B[Configure Git]
+B --> C[Install Helm]
+C --> D[Login to Chart Repository]
+D --> E[Build and push Charts + Push git tag]
+E --> F[Logout from Chart Repository]
+```
+
+## Example usage
+
+```yaml
+name: Release Charts
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  release:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v3
+        with:
+          fetch-depth: 0
+      - name: Run chart-releaser
+        uses: urbanmedia/helm-chart-releaser-action@latest
+        with:
+          gh_token: ${{ secrets.GITHUB_TOKEN }}
+          chart_repository_url: oci://ghcr.io/urbanmedia/helm-charts
+          chart_repository_username: ${{ secrets.GHCR_USERNAME }}
+          chart_repository_password: ${{ secrets.GHCR_PASSWORD }}
+```
---
+++ entrypoint.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+set -e
+if [[ -n "$DEBUG" ]]; then
+    set -x
+fi
+
+CHARTS_DIR=${CHARTS_DIR:-charts}
+CHART_REPOSITORY=${CHART_REPOSITORY:-""}
+
+lookup_latest_tag() {
+    git fetch --tags > /dev/null 2>&1
+    if ! git describe --tags --abbrev=0 2> /dev/null; then
+        git rev-list --max-parents=0 --first-parent HEAD
+    fi
+}
+
+filter_charts() {
+    while read -r chart; do
+        [[ ! -d "$chart" ]] && continue
+        local file="$chart/Chart.yaml"
+        if [[ -f "$file" ]]; then
+            echo "$chart"
+        else
+           echo "WARNING: $file is missing, assuming that '$chart' is not a Helm chart. Skipping." 1>&2
+        fi
+    done
+}
+
+lookup_chart_changes() {
+    local commit=$1
+    local charts_dir=$2
+    local changed_files
+    changed_files=$(git diff --find-renames --name-only "$commit" -- "$charts_dir")
+    local depth=$(( $(tr "/" "\n" <<< "$charts_dir" | sed '/^\(\.\)*$/d' | wc -l) + 1 ))
+    local fields="1-${depth}"
+    cut -d '/' -f "$fields" <<< "$changed_files" | uniq | filter_charts
+}
+
+package_chart() {
+    local charts=$1
+    for chart in $charts; do
+        echo "Packaging chart '$chart'..."
+        helm package "$chart" --dependency-update --destination $chart_destination_dir
+    done
+}
+
+create_git_tag_from_chart() {
+    local charts=$1
+    for chart in $charts; do
+        chart_version=$(echo $chart | sed -e 's+.tgz++g')
+        echo "Creating tag $chart_version..."
+        git tag $chart_version
+        git push origin $chart_version
+    done
+}
+
+# iterate over all built charts and push them to the chart repository
+push_chart() {
+    local charts=$1
+    for chart in $charts; do
+        echo "Pushing chart '$chart'..."
+        helm push ${chart_destination_dir}/$chart $CHART_REPOSITORY
+    done
+}
+
+# check if required environment variables are set
+if [[ -z "$CHART_REPOSITORY" ]]; then
+    echo "CHART_REPOSITORY is not set"
+    exit 1
+fi
+if [[ -z "$CHARTS_DIR" ]]; then
+    echo "CHARTS_DIR is not set"
+    exit 1
+fi
+
+
+# change the directory to the charts directory
+cd ${CHARTS_DIR}
+
+# the directory where the packaged charts will be stored
+chart_destination_dir="builds"
+
+# the last tag that was created
+# we use this to determine which charts have changed since the last release
+lastTag=$(lookup_latest_tag)
+# chart_diffs is a list of charts that have changed since the last release
+chart_diffs=$(lookup_chart_changes "$lastTag" "${CHARTS_DIR}")
+# package the changed charts
+package_chart "$chart_diffs"
+# create a tag for each chart
+create_git_tag_from_chart "$(ls $chart_destination_dir)"
+# push the charts to the chart repository
+push_chart "$(ls $chart_destination_dir)"
+
+# cleanup
+rm -rf $chart_destination_dir
